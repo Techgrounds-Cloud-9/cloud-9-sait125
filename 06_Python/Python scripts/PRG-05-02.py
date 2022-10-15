089fdf13140a1666831250270c7d28da7ceff6ab
sait125 <82023077+sait125@users.noreply.github.com>
2022-10-15 22:48:31 +0200

    Update PRG-05
    Added comments

--- 06_Python/Python scripts/PRG-05-02.py
+++ 06_Python/Python scripts/PRG-05-02.py
@@ -1,4 +1,4 @@
-while True:
+while True:  #using while True: is just a way of running a loop that will continue to run until you explicitly break out of it using break or return 
     x = float(input("Please input a number: "))
     if x > 100:
         print("Too high...")
