ee24049dd062cdb51f33ebce9fdba80d4777de53
sait125 <82023077+sait125@users.noreply.github.com>
2022-10-18 14:16:00 +0200

    Update PRG-07-02.py

--- 06_Python/Python scripts/PRG-07-02.py
+++ 06_Python/Python scripts/PRG-07-02.py
@@ -1,7 +1,8 @@
 list =[13, 6, 18, 25, 15]
 
+
 for i in range(len(list)):
-    if i == len(list)-1:
-        print (list[i] + list[0])
-    else:
+    if i < len(list)-1:
         print (list[i] + list[i+1])
+    else:
+        print (list[i] + list[0])
