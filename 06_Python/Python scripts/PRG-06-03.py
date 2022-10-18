ea9dba91ef21e4468d500f28b7f832de7de95f0c
sait125 <82023077+sait125@users.noreply.github.com>
2022-10-18 13:41:02 +0200

    PRG 6/7
    prg 6 list file removed
    changed extra line in 6-3
    finised 7-2

--- 06_Python/Python scripts/PRG-06-03.py
+++ 06_Python/Python scripts/PRG-06-03.py
@@ -1,7 +1,7 @@
-def avg():
+def avg(x,y):
     z = (x + y) / 2
     return z
-    
+
 x = 128
 y = 255
 z = avg(x,y)
--- 06_Python/Python scripts/PRG-07-02.py
+++ 06_Python/Python scripts/PRG-07-02.py
@@ -1,4 +1,7 @@
-mylist =[13, 6, 18, 25, 15]
-print(mylist)
-for i in mylist:
-    print(i)
+list =[13, 6, 18, 25, 15]
+
+for i in range(len(list)):
+    if i == len(list)-1:
+        print (list[i] + list[0])
+    else:
+        print (list[i] + list[i+1])
